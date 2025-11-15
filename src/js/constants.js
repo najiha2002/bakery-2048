c96e4a8436ed4d125a6c246e79e16a6682438a10
Najiha Zaharuddin <najiha.zaharuddin@bp.com>
2025-11-15 19:50:28 +0800

    define constants of game

---
+++ src/js/constants.js
@@ -0,0 +1,20 @@
+const GRID_SIZE = 4;
+const CANVAS_SIZE = 500;
+const TILE_SIZE = 100;
+const TILE_GAP = 15;
+// tbc
+const COLORS = {
+    2: '#fcefe6',
+    4: '#f2e8cb',
+    8: '#f5b682',
+    16: '#f29446',
+    32: '#ff775c',
+    64: '#e64c2e',
+    128: '#ede291',
+    256: '#fce130',
+    512: '#ffdb4a',
+    1024: '#f0b922',
+    2048: '#fad74d',
+};
+
+export { GRID_SIZE, TILE_SIZE, TILE_GAP, COLORS, CANVAS_SIZE };
