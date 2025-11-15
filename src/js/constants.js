826553d730f7b7df02b97a8f76b384fe5948b73d
Najiha Zaharuddin <najiha.zaharuddin@bp.com>
2025-11-15 20:20:19 +0800

    update colors of bg, and empty tile

--- src/js/constants.js
+++ src/js/constants.js
@@ -4,6 +4,12 @@ const TILE_SIZE = 100;
 const TILE_GAP = 15;
 // tbc
 const COLORS = {
+    background: '#faf8ef',
+    gridBackground: '#bbada0',
+    emptyTile: '#cdc1b4',
+    text: '#776e65',
+    brightText: '#f9f6f2',
+    0: '#cdc1b4',
     2: '#fcefe6',
     4: '#f2e8cb',
     8: '#f5b682',
@@ -15,6 +21,6 @@ const COLORS = {
     512: '#ffdb4a',
     1024: '#f0b922',
     2048: '#fad74d',
+    4096: '#3c3a32',
+    8192: '#3c3a32'
 };
-
-export { GRID_SIZE, TILE_SIZE, TILE_GAP, COLORS, CANVAS_SIZE };
